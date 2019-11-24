4a6abdff375d175046e53166bf26ea39e97e75ea
Amber <1980629811@qq.com>
2019-11-24 15:57:34 +0800

    1124-01

---
+++ 01-Primary/03-js/js/js09.js
@@ -0,0 +1,12 @@
+var oBox = document.getElementById('box');
+// function fn()
+// {
+// 	oBox.style.background='skyblue';
+// }
+// oBox.onclick=fn;
+// oBox.onclick = function(){
+// 	fn();
+// }
+oBox.onclick = function(){
+	oBox.style.background='skyblue';
+}
